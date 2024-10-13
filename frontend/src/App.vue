e5a2516672eaee695a469f8005ff0a969df070b8
zerosand1 <priyansh.23raj@gmail.com>
2024-10-13 15:47:09 +0530

    intial commit

---
+++ frontend/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1 class="text-3xl font-bold text-center mt-8">
+      Welcome to Your Vue.js + Tailwind CSS App
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+@import './assets/tailwind.css';
+</style>
